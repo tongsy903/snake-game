d27f2679d9748ddf2e6da305bc920f157c7b1d81
tongsy903 <tongsy903@gmail.com>
2026-09-04 23:55:58 +0800

    docs: 补充 LICENSE/README，代码版权署名改为 tongsy903
    README 附'从提交历史学习'导读：每个里程碑对应一个开发知识点；
    附带 git show/diff 自查命令与 PyCharm Log 查看方法

--- snake.py
+++ snake.py
@@ -4,9 +4,9 @@
 #
 # MIT License
 #
-# Copyright (c) 2026 Your Name
+# Copyright (c) 2026 tongsy903
 #
-# 使用本项目前，请把上面的 "Your Name" 改成你自己的名字或组织名称。
+# 使用本项目前，请把上面的版权行改成你自己的名字或组织名称。
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
